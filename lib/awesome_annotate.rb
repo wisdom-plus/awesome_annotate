652d0a9451762a91a7075db746318b3cfbc3ab42
wisdom-plus <tomoron326@outlook.jp>
2024-04-29 08:59:12 +0900

    init commit

---
+++ lib/awesome_annotate.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "awesome_annotate/version"
+
+module AwesomeAnnotate
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/awesome_annotate/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module AwesomeAnnotate
+  VERSION = "0.1.0"
+end
