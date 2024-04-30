7bf6d0a451403484661e54c16929395a50104066
wisdom-plus <tomoron326@outlook.jp>
2024-05-01 08:26:00 +0900

    fix:require enviroment

--- lib/awesome_annotate/cli.rb
+++ lib/awesome_annotate/cli.rb
@@ -13,6 +13,18 @@ module AwesomeAnnotate
 
     desc 'model [model name]', 'annotate your model'
     def model(model_name)
+      options = {}
+      OptionParser.new do |opts|
+        opts.on("-pPATH", "--path=PATH", "Path to the Rails application") do |path|
+          options[:path] = path
+        end
+      end.parse!
+
+      # abort "Rails application path is required" unless options[:path]
+      options[:path] = Dir.pwd unless options[:path]
+      p options[:path]
+      require File.join(options[:path], 'config', 'environment.rb')
+
       name = model_name.singularize.camelize
       klass = Object.const_get(name)
 
@@ -21,8 +33,12 @@ module AwesomeAnnotate
       column_names = klass.column_names
       model_dir = 'app/models'
       file_path = "#{model_dir}/#{model_name}.rb"
-      File.open(file_path, 'r') do |file|
-        file.puts "# Columns: #{column_names.join(', ')}"
+      file_arr = File.open(file_path).readlines
+      file_arr.unshift("# Columns: #{column_names.join(', ')}")
+      File.open(file_path, 'w') do |file|
+        file_arr.each do |line|
+          file.puts line
+        end
       end
       puts 'annotate your model'
     end
