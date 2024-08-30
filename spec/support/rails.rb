84c733de39d7e923863f17b248a5ca018a482440
wisdom-plus <tomoron326@outlook.jp>
2024-08-31 07:58:52 +0900

    add:rspec rails mock

---
+++ spec/support/rails.rb
@@ -0,0 +1,17 @@
+class ActionDispatch
+  class Routing
+    class RoutesInspector
+    end
+
+    class ConsoleFormatter
+      class Sheet
+      end
+    end
+
+    class RouteSet
+    end
+  end
+end
+
+class Rails
+end
