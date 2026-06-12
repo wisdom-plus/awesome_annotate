d2044d97440d0ef1966f383ce5675c7d6444e45c
wisdom-plus <wisdom-plus@users.noreply.github.com>
2026-06-13 08:32:31 +0900

    add:configration spec

---
+++ spec/lib/awesome_annotate/configuration_spec.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'tmpdir'
+
+RSpec.describe AwesomeAnnotate::Configuration do
+  describe '.load' do
+    it 'returns default options when the config file does not exist' do
+      config = described_class.load('missing.yml')
+
+      expect(config.options).to eq(
+        env_file_path: 'config/environment.rb',
+        model_dir: 'app/models',
+        route_file_path: 'config/routes.rb'
+      )
+    end
+
+    it 'loads supported options from a YAML config file' do
+      Dir.mktmpdir do |dir|
+        path = File.join(dir, 'awesome_annotate.yml')
+        File.write(path, <<~YAML)
+          env_file_path: spec/mock/config.rb
+          model_dir: spec/mock
+          route_file_path: spec/mock/routes.rb
+          unknown_option: ignored
+        YAML
+
+        config = described_class.load(path)
+
+        expect(config.options).to eq(
+          env_file_path: 'spec/mock/config.rb',
+          model_dir: 'spec/mock',
+          route_file_path: 'spec/mock/routes.rb'
+        )
+      end
+    end
+
+    it 'raises when the config file is not a YAML mapping' do
+      Dir.mktmpdir do |dir|
+        path = File.join(dir, 'awesome_annotate.yml')
+        File.write(path, "- value\n")
+
+        expect { described_class.load(path) }.to raise_error(
+          ArgumentError,
+          "Configuration file must contain a YAML mapping: #{path}"
+        )
+      end
+    end
+  end
+
+  describe '.create' do
+    it 'creates a default config file' do
+      Dir.mktmpdir do |dir|
+        path = File.join(dir, 'config/initializers/awesome_annotate.yml')
+
+        described_class.create(path)
+
+        expect(File.read(path)).to include('model_dir: app/models')
+      end
+    end
+  end
+end
