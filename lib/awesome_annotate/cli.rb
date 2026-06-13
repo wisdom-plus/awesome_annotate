88bb224757ae0f9a3e7806961870f864334953f3
wisdom-plus <wisdom-plus@users.noreply.github.com>
2026-06-14 08:32:01 +0900

    add:config command

--- lib/awesome_annotate/cli.rb
+++ lib/awesome_annotate/cli.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'awesome_annotate'
+require 'awesome_annotate/configuration'
 require 'awesome_annotate/model'
 require 'awesome_annotate/route'
 require 'awesome_annotate/version'
@@ -10,23 +11,37 @@ module AwesomeAnnotate
   class Remove < Thor
     desc 'model [model_name]', 'remove annotation from a model'
     def model(model_name)
-      AwesomeAnnotate::Model.new.remove(model_name)
+      model_annotator.remove(model_name)
     end
 
     desc 'models [model_names...]', 'remove annotations from all models or specified models'
     def models(*model_names)
-      AwesomeAnnotate::Model.new.remove_all(model_names)
+      model_annotator.remove_all(model_names)
     end
 
     desc 'routes', 'remove annotation from `config/routes.rb`'
     def routes
-      AwesomeAnnotate::Route.new.remove
+      route_annotator.remove
     end
 
     desc 'all', 'remove annotations from all models and routes'
     def all
-      AwesomeAnnotate::Model.new.remove_all
-      AwesomeAnnotate::Route.new.remove
+      model_annotator.remove_all
+      route_annotator.remove
+    end
+
+    private
+
+    def model_annotator
+      AwesomeAnnotate::Model.new(configuration.options)
+    end
+
+    def route_annotator
+      AwesomeAnnotate::Route.new(configuration.options)
+    end
+
+    def configuration
+      AwesomeAnnotate::Configuration.load
     end
   end
 
@@ -42,24 +57,35 @@ module AwesomeAnnotate
     map %w[model -m] => :model
     desc 'model [model_name]', 'annotate your model'
     def model(model_name)
-      AwesomeAnnotate::Model.new.annotate(model_name)
+      model_annotator.annotate(model_name)
     end
 
     desc 'models [model_names...]', 'annotate all models or specified models'
     def models(*model_names)
-      AwesomeAnnotate::Model.new.annotate_all(model_names)
+      model_annotator.annotate_all(model_names)
     end
 
     map %w[routes -r] => :routes
     desc 'routes', 'Writes application route information to `config/routes.rb`.'
     def routes
-      AwesomeAnnotate::Route.new.annotate
+      route_annotator.annotate
     end
 
     desc 'all', 'annotate all models and routes'
     def all
-      AwesomeAnnotate::Model.new.annotate_all
-      AwesomeAnnotate::Route.new.annotate
+      model_annotator.annotate_all
+      route_annotator.annotate
+    end
+
+    desc 'init', "create #{AwesomeAnnotate::Configuration::DEFAULT_PATH}"
+    def init
+      path = AwesomeAnnotate::Configuration::DEFAULT_PATH
+      if File.exist?(path)
+        say "Config file already exists: #{path}"
+      else
+        AwesomeAnnotate::Configuration.create(path)
+        say "create #{path}"
+      end
     end
 
     desc 'remove SUBCOMMAND', 'remove generated annotations'
@@ -67,6 +93,20 @@ module AwesomeAnnotate
 
     def self.exit_on_failure?
       true
+    end
+
+    private
+
+    def model_annotator
+      AwesomeAnnotate::Model.new(configuration.options)
+    end
+
+    def route_annotator
+      AwesomeAnnotate::Route.new(configuration.options)
+    end
+
+    def configuration
+      AwesomeAnnotate::Configuration.load
     end
   end
 end
